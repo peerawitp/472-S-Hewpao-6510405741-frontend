86faf7ec093f7349a43f0acd740d4e651332af2f
xeff09 <wongchaita03@gmail.com>
2025-03-13 21:52:00 +0700

    merge3

--- src/app/component/ProductCard.tsx
+++ src/app/component/ProductCard.tsx
@@ -9,48 +9,6 @@ function ProductCard() {
   const isTravelerPage = pathname.includes("/product-requests");
   console.log("isTravelerPage", isTravelerPage);
 
-<<<<<<< HEAD
-  return (
-    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
-      {products.map((product) => (
-        <div
-          key={product.id}
-          className="border rounded-lg p-4 shadow-sm bg-white hover:shadow-lg transition-all duration-200 flex flex-col h-full"
-        >
-          {/* Product Image */}
-          <div className="w-full h-48 flex items-center justify-center bg-[#00000000]">
-            <Image
-              src={product.image}
-              alt={product.name}
-              width={180}
-              height={180}
-              className="rounded object-contain"
-            />
-          </div>
-
-          {/* Product Info */}
-          <h2 className="mt-4 font-semibold text-gray-800">{product.name}</h2>
-
-          <div className="mt-auto">
-            <div className="mt-2 text-sm">
-              <div className="flex items-center text-gray-600">
-                <span className="font-medium">From:</span>
-                <span className="ml-2">{product.product_from}</span>
-              </div>
-              <div className="flex items-center text-gray-600 mt-1 mb-2">
-                <span className="font-medium">To:</span>
-                <span className="ml-2">{product.product_to}</span>
-              </div>
-            </div>
-            <p className="text-gray-600 mb-2">
-              Retailer price:{" "}
-              <span className="font-bold text-gray-900">{product.price}</span>
-            </p>
-
-            {isTravelerPage && (
-              <div className="mt-3">
-                <MakeOfferButton productRequestID={Number(product.id)} />
-=======
   const { data: prods2, isLoading: loadingProds2 } =
     useGetPaginatedProductRequests(1, 5);
 
@@ -97,12 +55,7 @@ function ProductCard() {
 
             {isTravelerPage && (
               <div className="mt-3">
-<<<<<<< HEAD
                 <MakeOfferButton productRequestID={product.id} />
->>>>>>> 1203b79 (init: all getting prods usage)
-=======
-                <MakeOfferButton />
->>>>>>> 6f4cfd4 (rebase: from this branch)
               </div>
             )}
           </div>
--- src/interfaces/Offer.ts
+++ src/interfaces/Offer.ts
@@ -1,18 +1,11 @@
-<<<<<<< HEAD
-=======
 import { Product_Request } from "./Product-Request";
->>>>>>> 4b01bd4 (rebase: from this branch)
 import { User } from "./User";
 
 export interface Offer {
   id: string;
   product_request_id: string;
-<<<<<<< HEAD
-=======
   product_request?: Product_Request;
->>>>>>> 4b01bd4 (rebase: from this branch)
   user_id: string;
   user?: User;
   offer_date: string;
-    
 }
