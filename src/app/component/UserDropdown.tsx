596ea704ffb88d2aba6f0fb24d2ee20857079c2c
MarchKie <marchaod123.com@gmail.com>
2025-03-14 15:20:15 +0700

    add-verification-btn

--- src/app/component/UserDropdown.tsx
+++ src/app/component/UserDropdown.tsx
@@ -54,21 +54,26 @@ const UserDropdown = ({ email }: UserDropdownProps) => {
             <p className="text-sm font-medium text-gray-900">Signed in as</p>
             <p className="text-xs text-gray-600 truncate">{email}</p>
           </div>
-          
+
           <Link href="/my-product">
             <div className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">
               My Products
             </div>
           </Link>
-          
+
           <Link href="/my-offer">
             <div className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">
               My Offers
             </div>
           </Link>
-          
+
+          <Link href="/verification">
+            <div className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">
+              Become a Traveler
+            </div>
+          </Link>
           <div className="border-t border-gray-200 mt-1"></div>
-          
+
           <button
             onClick={() => signOut()}
             className="block w-full text-left px-4 py-2 text-sm text-red-600 hover:bg-gray-100"
